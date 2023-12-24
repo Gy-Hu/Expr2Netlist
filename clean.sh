55e71ff33e3bbe578c5b66e7c1f4d3bc79a836aa
GaryHU <swe1609011@gmail.com>
2023-12-24 18:19:46 +0800

    update scripts, fix typos and bugs, update run shell

---
+++ clean.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Define the directory to move files to
+trash_dir="./.trash"
+
+# Check if the trash directory exists, if not, create it
+if [ ! -d "$trash_dir" ]; then
+    mkdir "$trash_dir"
+fi
+
+# List of files to move to trash
+files_to_trash=(
+    "ABC_opt.blif"
+    "area.log"
+    "EqnGenerator"
+    "expr2eqn"
+    "expr2eqn.hi"
+    "expr2eqn.o"
+    "NANDNOR.lib"
+    "opt.v"
+    "output.eqn"
+)
+
+# Loop through the files and move them to the trash directory
+for file in "${files_to_trash[@]}"; do
+    if [ -e "$file" ]; then
+        mv "$file" "$trash_dir"
+        echo "Moved $file to $trash_dir."
+    else
+        echo "Warning: $file does not exist and cannot be moved."
+    fi
+done
+
+echo "Trash operation complete."
--- run.sh
+++ run.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-# Define the Haskell source file and the output binary
+# Define the Haskell source file, the output binary, and default output file
 HASKELL_SRC="expr2eqn.hs"
 OUTPUT_BIN="EqnGenerator"
-INPUT_EXPR="a && b"  # The expression to pass to the program
-OUTPUT_FILE="output.eqn"  # The file to save the EQN to
+OUTPUT_FILE="output.eqn"  # Default EQN save path
+
+# Ask the user for the expression
+read -p "Enter the expression: " INPUT_EXPR
 
 # Check if the Haskell compiler (GHC) is installed
-if ! command -v ghc &> /dev/null
-then
+if ! command -v ghc &> /dev/null; then
     echo "GHC could not be found. Please install GHC to compile Haskell programs."
     exit 1
 fi
@@ -30,6 +31,61 @@ $INPUT_EXPR
 $OUTPUT_FILE
 EOF
 
-echo "EQN saved to $OUTPUT_FILE"
+# Choose optimization tool
+echo "Choose the optimization tool:"
+echo "1) ABC"
+echo "2) SIS"
+read -p "Select an option (1/2): " TOOL_CHOICE
+
+BLIF_PATH=""  # Initialize BLIF_PATH variable
+
+case $TOOL_CHOICE in
+    1)
+        echo "Optimizing with ABC..."
+        abc -f scripts/abc_opt.script
+        BLIF_PATH="ABC_opt.blif"  # Set BLIF path for ABC
+        ;;
+    2)
+        echo "Optimizing with SIS..."
+        # Run the SIS script with the specified commands and then quit
+        ./logic-synthesis-SIS/build/bin/sis <<EOF
+read_eqn $OUTPUT_FILE
+sweep; eliminate -1
+simplify -m nocomp
+eliminate -1
+sweep; eliminate 5 
+simplify -m nocomp
+resub -a
+fx
+resub -a; sweep
+eliminate -1; sweep
+full_simplify -m nocomp
+write_blif sis_opt.blif
+quit
+EOF
+        echo "Running ABC mapping post SIS optimization..."
+        abc -f scripts/abc_map.script
+        BLIF_PATH="sis_opt.blif"  # Set BLIF path for SIS
+        ;;
+    *)
+        echo "Invalid option selected."
+        exit 1
+        ;;
+esac
+
+# Display the area cost
+echo "Calculating the area cost..."
+AREA_COST=$(./OpenTimer/bin/ot-shell -i scripts/print_area.conf | tail -n 1)  # Capture only the last line
+
+# Assuming the path to the netlist is known and fixed
+NETLIST_PATH="opt.v"
+
+echo "Optimization and area calculation completed."
+
+echo "Area Cost: $AREA_COST"  # Displaying the area cost
+# echo "EQN saved to $OUTPUT_FILE"
+echo "Saved netlist path: $NETLIST_PATH"
+echo "BLIF file path: $BLIF_PATH"  # Print the BLIF path based on the optimization tool selected
+echo "EQN file path: $OUTPUT_FILE"
 
 # End of script
